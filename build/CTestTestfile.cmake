e7e98942883e111089af8a3aff70e302c2646721
Mayuresh Satao <msatao75@gmail.com>
2026-01-16 17:19:52 -0500

    added write ahead logs

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mayureshsatao/Documents/GithubRepo/LSM-Tree-Storage-Engine
+# Build directory: /Users/mayureshsatao/Documents/GithubRepo/LSM-Tree-Storage-Engine/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(memtable_test "/Users/mayureshsatao/Documents/GithubRepo/LSM-Tree-Storage-Engine/build/memtable_test")
+set_tests_properties(memtable_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/mayureshsatao/Documents/GithubRepo/LSM-Tree-Storage-Engine/CMakeLists.txt;27;add_test;/Users/mayureshsatao/Documents/GithubRepo/LSM-Tree-Storage-Engine/CMakeLists.txt;0;")
