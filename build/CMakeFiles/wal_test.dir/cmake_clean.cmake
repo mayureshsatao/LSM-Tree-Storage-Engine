f161d10aa3e97e239b490fda8c77a5ecdd20c743
Mayuresh Satao <msatao75@gmail.com>
2026-01-16 17:20:19 -0500

    added write ahead logs

---
+++ build/CMakeFiles/wal_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wal_test.dir/test/wal_test.cpp.o"
+  "CMakeFiles/wal_test.dir/test/wal_test.cpp.o.d"
+  "wal_test"
+  "wal_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wal_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
