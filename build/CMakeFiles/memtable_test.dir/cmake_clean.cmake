407dd86214d3760400a82080d325744c2eaa50c1
Mayuresh Satao <msatao75@gmail.com>
2026-01-16 17:19:52 -0500

    added write ahead logs

---
+++ build/CMakeFiles/memtable_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memtable_test.dir/test/memtable_test.cpp.o"
+  "CMakeFiles/memtable_test.dir/test/memtable_test.cpp.o.d"
+  "memtable_test"
+  "memtable_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/memtable_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
